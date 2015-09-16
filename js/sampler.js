19d0a747eccb77d4633e12cd5dd98d9fded86177
Kenny Ma <kenny.uk@me.com>
2015-09-17 01:39:38 +0800

    增加了sampler.js文件

---
+++ js/sampler.js
@@ -0,0 +1,14 @@
+/**
+Layout Created By Kenny Ma on 17/09/2015
+Maintained by Stella
+**/
+
+/**
+@param start_time 样本开始的时间
+@param end_time 样本结束的时间
+@sample name 用ajax加载的sample 的文件名
+@param is_loop 是trigger一次还是循环
+**/
+function sampler_trigger(start_time,end_time,sample_name,is_loop){
+//TO-DO
+}
